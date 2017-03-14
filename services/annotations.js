fa974aceba7e2d977af89956dca6478377705150
aw3s0me <akorovin00@gmail.com>
2017-03-14 22:01:24 +0100

    refactored suggestion annotation service

--- services/annotations.js
+++ services/annotations.js
@@ -7,27 +7,43 @@ export default {
     name: 'annotations',
     // At least one of the CRUD methods is Required
     read: (req, resource, params, config, callback) => {
-        if (!params.text) {
-            callback(null, {success: false, results: {}});
-        }
 
-        console.log(params.text);
-        let url = 'http://spotlight.sztaki.hu:2222/rest/annotate?text=';
-        url = url + encodeURI(params.text) + '&confidence=0.2&support=20';
-        let a = 1;
-        rp.post({
-            uri: url,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).then((res) => {
-            callback(null, {
-                success: true,
-                results: JSON.parse(JSON.stringify(res || null ))
-            });
-        }).catch((err) => {
-            callback(null, {success: false, results: {}});
-        });
+        if (resource === 'annotations.suggestions') {
+            processSuggestions(params, callback);
+        } else if (resource === 'annotations.wikipedia') {
+            getWikipediaLinks(params, callback);
+        }
     }
 };
+
+function processSuggestions(params, callback) {
+    if (!params.text) {
+        callback(null, {success: false, results: {}});
+    }
+    
+    let url = 'http://spotlight.sztaki.hu:2222/rest/annotate?text=';
+    url = url + encodeURI(params.text) + '&confidence=0.4&support=20';
+
+    rp.post({
+        uri: url,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then((res) => {
+        callback(null, {
+            success: true,
+            results: JSON.parse(JSON.stringify(res || null ))
+        });
+    }).catch((err) => {
+        callback(null, {success: false, results: {}});
+    });
+}
+
+function getWikipediaLinks(params, callback) {
+    if (!params.links) {
+        callback(null, {success: false, results: {}});
+    }
+
+
+}
