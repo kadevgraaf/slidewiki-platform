02eb22aae7ff2e2404382e86ffa2dc3ad852a29f
aw3s0me <akorovin00@gmail.com>
2017-03-29 20:21:18 +0200

    searching by keywords backbone get method

--- services/semsearch.js
+++ services/semsearch.js
@@ -25,6 +25,25 @@ export default {
 };
 
 function searchByKeywords(params, callback) {
+    if (!params.keywords) {
+        callback(null, {success: false, results: {}});
+        return;
+    }
+    
+    const { search } = Microservices;
+    const uri = search.uri + '/search?' + formQueryParams(params.keywords);
+    rp.get({
+        uri: uri
+    }).then(results => {
+        // TODO: parse results
+        callback(null, {
+            success: true,
+            results: JSON.parse(JSON.stringify(results))
+        });
+    });
+}
+
+function formQueryParams(keywords) {
     
 }
 
